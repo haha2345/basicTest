9cf848d450f65c07129fb76bc0ca8f3c1993b5ee
王文哲 <woden3702@aliyun.com>
2020-07-30 10:01:21 +0800

    删除了视频上传的提示信息

--- app/src/main/java/com/example/basictest/Activity/CameraActivity.java
+++ app/src/main/java/com/example/basictest/Activity/CameraActivity.java
@@ -31,14 +31,15 @@ import static com.example.basictest.utils.DataCleanManager.clear;
 public class CameraActivity extends BaseCameraActivity {
 
 
-    String name,phone,idcard;
+    String name, phone, idcard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        name=getIntent().getStringExtra("name");
-        idcard=getIntent().getStringExtra("idcard");
-        phone=getIntent().getStringExtra("phone");
+        name = getIntent().getStringExtra("name");
+        idcard = getIntent().getStringExtra("idcard");
+        phone = getIntent().getStringExtra("phone");
         tipDialog = new QMUITipDialog.Builder(CameraActivity.this)
                 .setIconType(QMUITipDialog.Builder.ICON_TYPE_FAIL)
                 .setTipWord("录制失败，请重新录制")
@@ -52,14 +53,14 @@ public class CameraActivity extends BaseCameraActivity {
             @Override
             public void onClick(View view) {
                 handler.removeCallbacks(runnable);
-                showProgressDialog(CameraActivity.this,"请稍后");
+                showProgressDialog(CameraActivity.this, "请稍后");
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         //要延时的程序
                         uploadVideo();
                     }
-                },2000); //8000为毫秒单位
+                }, 2000); //8000为毫秒单位
                 //dismissProgressDialog();
 
 //                Intent intent=new Intent(getBaseContext(), Apply3Activity.class);
@@ -67,78 +68,73 @@ public class CameraActivity extends BaseCameraActivity {
 //                intent.putExtra("imagepath",imagePath);
 //                intent.putExtra("base64str",src);
 //                startActivity(intent);
-                Log.d("测试","aaa");
+                Log.d("测试", "aaa");
 
             }
         });
 
 
-
-
-
     }
 
     //拦截器
-    public void breaker(Context mContext){
+    public void breaker(Context mContext) {
         clear(mContext);
-        Intent intent=new Intent(mContext, LoginActivity.class);
+        Intent intent = new Intent(mContext, LoginActivity.class);
         //调到页面，关闭之前所有页面
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         mContext.startActivity(intent);
     }
 
 
-    public void uploadVideo(){
-        token= SpUtils.getInstance(this).getString("token",null);
-        caseId=SpUtils.getInstance(this).getString("caseId",null);
-        filepath=SpUtils.getInstance(this).getString("videopath",null);
-        videoFile=new File(filepath);
+    public void uploadVideo() {
+        token = SpUtils.getInstance(this).getString("token", null);
+        caseId = SpUtils.getInstance(this).getString("caseId", null);
+        filepath = SpUtils.getInstance(this).getString("videopath", null);
+        videoFile = new File(filepath);
 
-        if (videoFile.exists()){
-            if (!IsFileInUse(filepath)){
+        if (videoFile.exists()) {
+            if (!IsFileInUse(filepath)) {
                 HttpRequest.build(CameraActivity.this, netConstant.getUploadNotarizeVideo())
                         .setMediaType(baseokhttp3.MediaType.parse("video/mpeg4"))
-                        .addHeaders("Authorization","Bearer "+token)
-                        .addHeaders("Content-Type","multipart/form-data")
-                        .addParameter("notarizeVideoFile",videoFile)
-                        .addParameter("caseId",caseId)
+                        .addHeaders("Authorization", "Bearer " + token)
+                        .addHeaders("Content-Type", "multipart/form-data")
+                        .addParameter("notarizeVideoFile", videoFile)
+                        .addParameter("caseId", caseId)
                         .setJsonResponseListener(new JsonResponseListener() {
                             @Override
                             public void onResponse(JsonMap main, Exception error) {
-                                dismissProgressDialog();
-                                if (error!=null){
-                                    Log.e("上传","连接失败",error);
+//                                if (error!=null){
+//                                    Log.e("上传","连接失败",error);
+//                                    dismissProgressDialog();
+//                                    Toast.makeText(CameraActivity.this,"连接失败,请重试",Toast.LENGTH_SHORT).show();
+//                                }else {
+                                if (main.getString("code").equals("200")) {
+                                    //上传成功
+                                    Intent intent = new Intent(getBaseContext(), Apply3Activity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    intent.putExtra("name", name);
+                                    intent.putExtra("idcard", idcard);
+                                    intent.putExtra("phone", phone);
+                                    intent.putExtra("videopath", filepath);
+                                    intent.putExtra("imagepath", imagePath);
+                                    intent.putExtra("base64str", src);
+                                    startActivity(intent);
                                     dismissProgressDialog();
-                                    Toast.makeText(CameraActivity.this,"连接失败,请重试",Toast.LENGTH_SHORT).show();
-                                }else {
-                                    if (main.getString("code").equals("200")){
-                                        //上传成功
-                                        Intent intent=new Intent(getBaseContext(), Apply3Activity.class);
-                                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                        intent.putExtra("name",name);
-                                        intent.putExtra("idcard",idcard);
-                                        intent.putExtra("phone",phone);
-                                        intent.putExtra("videopath",filepath);
-                                        intent.putExtra("imagepath",imagePath);
-                                        intent.putExtra("base64str",src);
-                                        startActivity(intent);
-                                        dismissProgressDialog();
-                                    } else if (main.getString("code").equals("401")){
-                                        dismissProgressDialog();
-                                        breaker(CameraActivity.this);
-                                    }else {
-                                        Log.e("上传",main.getString("msg"));
-                                        Log.e("上传",main.getString("code"));
-                                        dismissProgressDialog();
+                                } else if (main.getString("code").equals("401")) {
+                                    dismissProgressDialog();
+                                    breaker(CameraActivity.this);
+                                } else {
+                                    Log.e("上传", main.getString("msg"));
+                                    Log.e("上传", main.getString("code"));
+                                    dismissProgressDialog();
+                                    Toast.makeText(CameraActivity.this, main.getString("msg"), Toast.LENGTH_SHORT).show();
 
-                                        Toast.makeText(CameraActivity.this,main.getString("msg"),Toast.LENGTH_SHORT).show();
-
-                                    }
                                 }
                             }
+//                            }
                         })
                         .doPost();
-            }else {
+            } else {
                 //文件正在被操作
                 Log.d("a", "uploadVideo: 文件正在被操作");
                 new Handler().postDelayed(new Runnable() {
@@ -151,9 +147,9 @@ public class CameraActivity extends BaseCameraActivity {
                 }, 2000);    //延时1s执行
             }
 
-        }else {
+        } else {
 
-            Toast.makeText(CameraActivity.this,"录制失败，请重新录制",Toast.LENGTH_SHORT).show();
+            Toast.makeText(CameraActivity.this, "录制失败，请重新录制", Toast.LENGTH_SHORT).show();
 
             tv_camera_timer.setText("00:00");
             uploadBtn.setVisibility(View.INVISIBLE);
@@ -163,10 +159,11 @@ public class CameraActivity extends BaseCameraActivity {
 
 
     }
-    private void deleteVideo(File file){
-        if (file.exists()){
+
+    private void deleteVideo(File file) {
+        if (file.exists()) {
             file.delete();
-        }else{
+        } else {
             Log.d("删除", "deletePdf: 删除失败");
         }
     }
